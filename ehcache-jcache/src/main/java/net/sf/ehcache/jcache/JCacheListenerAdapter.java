ab70d9c2073b188f7c0f42d01a37a974cca72ee0
Ryan Gardner <ryan.gardner@dealer.com>
2011-11-04 13:48:12 -0400

    Implement a cache listener adapter to wrap EhCache's listeners

---
+++ ehcache-jcache/src/main/java/net/sf/ehcache/jcache/JCacheListenerAdapter.java
@@ -0,0 +1,257 @@
+package net.sf.ehcache.jcache;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
+
+import javax.cache.Cache;
+import javax.cache.event.CacheEntryCreatedListener;
+import javax.cache.event.CacheEntryExpiredListener;
+import javax.cache.event.CacheEntryListener;
+import javax.cache.event.CacheEntryRemovedListener;
+import javax.cache.event.CacheEntryUpdatedListener;
+
+
+public class JCacheListenerAdapter<K, V> implements CacheEventListener {
+    private CacheEntryListener<K, V> cacheListener;
+    private JCache<K, V> jcache;
+    private boolean removedListener;
+    private boolean createdListener;
+    private boolean updatedListener;
+    private boolean expiredListener;
+
+    public JCacheListenerAdapter(CacheEntryListener<K, V> cacheListener) {
+        this.cacheListener = cacheListener;
+        removedListener = implementsMethods(CacheEntryRemovedListener.class);
+        createdListener = implementsMethods(CacheEntryCreatedListener.class);
+        updatedListener = implementsMethods(CacheEntryUpdatedListener.class);
+        expiredListener = implementsMethods(CacheEntryExpiredListener.class);
+    }
+
+    private boolean implementsMethods(Class cls) {
+        return cacheListener.getClass().isAssignableFrom(cls);
+    }
+
+    /**
+     * Called immediately after an attempt to remove an element. The remove method will block until
+     * this method returns.
+     * <p/>
+     * This notification is received regardless of whether the cache had an element matching
+     * the removal key or not. If an element was removed, the element is passed to this method,
+     * otherwise a synthetic element, with only the key set is passed in.
+     * <p/>
+     * This notification is not called for the following special cases:
+     * <ol>
+     * <li>removeAll was called. See {@link #notifyRemoveAll(net.sf.ehcache.Ehcache)}
+     * <li>An element was evicted from the cache.
+     * See {@link #notifyElementEvicted(net.sf.ehcache.Ehcache, net.sf.ehcache.Element)}
+     * </ol>
+     *
+     * @param cache   the cache emitting the notification
+     * @param element the element just deleted, or a synthetic element with just the key set if
+     *                no element was removed.
+     */
+    @Override
+    public void notifyElementRemoved(Ehcache cache, Element element) throws CacheException {
+        if (removedListener) {
+            if (element != null) {
+                ((CacheEntryRemovedListener<K, V>) cacheListener).onRemove(new JCacheEntry(element));
+            }
+        }
+    }
+
+    /**
+     * Called immediately after an element has been put into the cache. The
+     * {@link net.sf.ehcache.Cache#put(net.sf.ehcache.Element)} method
+     * will block until this method returns.
+     * <p/>
+     * Implementers may wish to have access to the Element's fields, including value, so the
+     * element is provided. Implementers should be careful not to modify the element. The
+     * effect of any modifications is undefined.
+     *
+     * @param cache   the cache emitting the notification
+     * @param element the element which was just put into the cache.
+     */
+    @Override
+    public void notifyElementPut(Ehcache cache, Element element) throws CacheException {
+        if (createdListener) {
+            if (element != null) {
+                ((CacheEntryCreatedListener<K, V>) cacheListener).onCreate(new JCacheEntry<K, V>(element));
+            }
+        }
+    }
+
+    /**
+     * Called immediately after an element has been put into the cache and the element already
+     * existed in the cache. This is thus an update.
+     * <p/>
+     * The {@link net.sf.ehcache.Cache#put(net.sf.ehcache.Element)} method
+     * will block until this method returns.
+     * <p/>
+     * Implementers may wish to have access to the Element's fields, including value, so the
+     * element is provided. Implementers should be careful not to modify the element. The
+     * effect of any modifications is undefined.
+     *
+     * @param cache   the cache emitting the notification
+     * @param element the element which was just put into the cache.
+     */
+    @Override
+    public void notifyElementUpdated(Ehcache cache, Element element) throws CacheException {
+        if (updatedListener) {
+            if (element != null) {
+                ((CacheEntryCreatedListener<K, V>) cacheListener).onCreate(new JCacheEntry<K, V>(element));
+            }
+        }
+    }
+
+    /**
+     * Called immediately after an element is <i>found</i> to be expired. The
+     * {@link net.sf.ehcache.Cache#remove(Object)} method will block until this method returns.
+     * <p/>
+     * As the {@link net.sf.ehcache.Element} has been expired, only what was the key of the element is known.
+     * <p/>
+     * Elements are checked for expiry in ehcache at the following times:
+     * <ul>
+     * <li>When a get request is made
+     * <li>When an element is spooled to the diskStore in accordance with a MemoryStore
+     * eviction policy
+     * <li>In the DiskStore when the expiry thread runs, which by default is
+     * {@link net.sf.ehcache.Cache#DEFAULT_EXPIRY_THREAD_INTERVAL_SECONDS}
+     * </ul>
+     * If an element is found to be expired, it is deleted and this method is notified.
+     *
+     * @param cache   the cache emitting the notification
+     * @param element the element that has just expired
+     *                <p/>
+     *                Deadlock Warning: expiry will often come from the <code>DiskStore</code>
+     *                expiry thread. It holds a lock to the DiskStorea the time the
+     *                notification is sent. If the implementation of this method calls into a
+     *                synchronized <code>Cache</code> method and that subsequently calls into
+     *                DiskStore a deadlock will result. Accordingly implementers of this method
+     *                should not call back into Cache.
+     */
+    @Override
+    public void notifyElementExpired(Ehcache cache, Element element) {
+        if (expiredListener) {
+            if (element != null) {
+                ((CacheEntryExpiredListener<K, V>) cacheListener).onExpire(new JCacheEntry<K, V>(element));
+            }
+        }
+    }
+
+    /**
+     * Called immediately after an element is evicted from the cache. Evicted in this sense
+     * means evicted from one store and not moved to another, so that it exists nowhere in the
+     * local cache.
+     * <p/>
+     * In a sense the Element has been <i>removed</i> from the cache, but it is different,
+     * thus the separate notification.
+     *
+     * @param cache   the cache emitting the notification
+     * @param element the element that has just been evicted
+     */
+    @Override
+    public void notifyElementEvicted(Ehcache cache, Element element) {
+        if (expiredListener) {
+            if (element != null) {
+                ((CacheEntryExpiredListener<K, V>) cacheListener).onExpire(new JCacheEntry<K, V>(element));
+            }
+        }
+    }
+
+    /**
+     * Called during {@link net.sf.ehcache.Ehcache#removeAll()} to indicate that the all
+     * elements have been removed from the cache in a bulk operation. The usual
+     * {@link #notifyElementRemoved(net.sf.ehcache.Ehcache, net.sf.ehcache.Element)}
+     * is not called.
+     * <p/>
+     * This notification exists because clearing a cache is a special case. It is often
+     * not practical to serially process notifications where potentially millions of elements
+     * have been bulk deleted.
+     *
+     * @param cache the cache emitting the notification
+     */
+    @Override
+    public void notifyRemoveAll(Ehcache cache) {
+        if (removedListener) {
+            // TODO - does ehCache have the ability to pass this up natively? If not, decorating over the native events might not work            
+            // and we might need to have our JCache adapter layer handle talking to CacheEntryListeners directly (which wouldn't be 
+            // ideal because then only things happening through the JCache wrapper would be sent out through the event system)
+            ((CacheEntryRemovedListener<K, V>) cacheListener).onRemoveAll(jcache);
+        }
+    }
+
+    /**
+     * Give the listener a chance to cleanup and free resources when no longer needed
+     */
+    @Override
+    public void dispose() {
+        this.jcache = null;
+    }
+
+    /**
+     * Creates and returns a copy of this object.  The precise meaning
+     * of "copy" may depend on the class of the object. The general
+     * intent is that, for any object {@code x}, the expression:
+     * <blockquote>
+     * <pre>
+     * x.clone() != x</pre></blockquote>
+     * will be true, and that the expression:
+     * <blockquote>
+     * <pre>
+     * x.clone().getClass() == x.getClass()</pre></blockquote>
+     * will be {@code true}, but these are not absolute requirements.
+     * While it is typically the case that:
+     * <blockquote>
+     * <pre>
+     * x.clone().equals(x)</pre></blockquote>
+     * will be {@code true}, this is not an absolute requirement.
+     *
+     * By convention, the returned object should be obtained by calling
+     * {@code super.clone}.  If a class and all of its superclasses (except
+     * {@code Object}) obey this convention, it will be the case that
+     * {@code x.clone().getClass() == x.getClass()}.
+     *
+     * By convention, the object returned by this method should be independent
+     * of this object (which is being cloned).  To achieve this independence,
+     * it may be necessary to modify one or more fields of the object returned
+     * by {@code super.clone} before returning it.  Typically, this means
+     * copying any mutable objects that comprise the internal "deep structure"
+     * of the object being cloned and replacing the references to these
+     * objects with references to the copies.  If a class contains only
+     * primitive fields or references to immutable objects, then it is usually
+     * the case that no fields in the object returned by {@code super.clone}
+     * need to be modified.
+     *
+     * The method {@code clone} for class {@code Object} performs a
+     * specific cloning operation. First, if the class of this object does
+     * not implement the interface {@code Cloneable}, then a
+     * {@code CloneNotSupportedException} is thrown. Note that all arrays
+     * are considered to implement the interface {@code Cloneable} and that
+     * the return type of the {@code clone} method of an array type {@code T[]}
+     * is {@code T[]} where T is any reference or primitive type.
+     * Otherwise, this method creates a new instance of the class of this
+     * object and initializes all its fields with exactly the contents of
+     * the corresponding fields of this object, as if by assignment; the
+     * contents of the fields are not themselves cloned. Thus, this method
+     * performs a "shallow copy" of this object, not a "deep copy" operation.
+     *
+     * The class {@code Object} does not itself implement the interface
+     * {@code Cloneable}, so calling the {@code clone} method on an object
+     * whose class is {@code Object} will result in throwing an
+     * exception at run time.
+     *
+     * @return a clone of this instance.
+     * @throws CloneNotSupportedException if the object's class does not
+     *                                    support the {@code Cloneable} interface. Subclasses
+     *                                    that override the {@code clone} method can also
+     *                                    throw this exception to indicate that an instance cannot
+     *                                    be cloned.
+     * @see Cloneable
+     */
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
